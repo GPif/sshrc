ddfabb34c2bbdf9eba52708bd1a1a9a68c6092a6
GPif <gpifmail@yahoo.fr>
2014-08-29 20:58:06 +0200

    Fully working application. The power off use ssh connection with credential set in the settings.

--- SSHRC/src/com/gpif/sshrc/MainActivity.java
+++ SSHRC/src/com/gpif/sshrc/MainActivity.java
@@ -1,5 +1,8 @@
 package com.gpif.sshrc;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Properties;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,11 +15,18 @@ import android.view.View;
 import android.widget.Button;
 
 import com.gpif.utils.WOL;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 public class MainActivity extends Activity {
 
 	protected String ip;
 	protected String mac;
+	
+	protected String user;
+	protected String password;
+	protected String host;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,21 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				new RunWol().execute(ip,mac);
+			}
+		});
+
+		this.user = sharedPrefs.getString("ssh_user", "-1");
+		this.password = sharedPrefs.getString("ssh_password", "-1");		
+		this.host = sharedPrefs.getString("ssh_host", "-1");		
+		
+		Button poff = (Button) findViewById(R.id.button_pof);
+		
+		poff.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				new RunSSHCmd().execute(user, password, host);
+				
 			}
 		});
 
@@ -80,5 +105,53 @@ public class MainActivity extends Activity {
 		}
 		
 	}
+	
+	private class RunSSHCmd extends AsyncTask<String, Void, Void> {
+		@Override	
+		protected Void doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			try {
+				executeRemoteCommand(params[0], params[1], params[2], 22);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		public String executeRemoteCommand(
+	            String username,
+	            String password,
+	            String hostname,
+	            int port) throws Exception {    
+
+				JSch jsch = new JSch();
+				Session session = jsch.getSession(username, hostname, 22);
+				session.setPassword(password);
+				
+				// Avoid asking for key confirmation
+				Properties prop = new Properties();
+				prop.put("StrictHostKeyChecking", "no");
+				System.out.println("Send ssh command");
+				session.setConfig(prop);
+				
+				session.connect();
+				
+				// SSH Channel
+				ChannelExec channelssh = (ChannelExec)
+				                session.openChannel("exec");      
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				channelssh.setOutputStream(baos);
+				
+				// Execute command
+				channelssh.setCommand("echo "+password+" | sudo -S poweroff");
+				channelssh.connect();        
+				channelssh.disconnect();
+				
+				return baos.toString();
+			}
+
+		
+	 }
 	
 }
